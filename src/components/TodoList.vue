86a88a2c5dcb062688c0a32d8c827458b0383d7b
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2017-08-23 17:10:05 -0400

    Added Deleting method for Todo component

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,38 +1,29 @@
 <template>
   <div>
-    // JavaScript expressions in Vue are enclosed in double curly brackets.
     <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
     <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-    <div class='ui centered card' v-for="todo in todos">
-      <div class='content'>
-        <div class='header'>
-          {{ todo.title }}
-        </div>
-        <div class='meta'>
-          {{ todo.project }}
-        </div>
-        <div class='extra content'>
-          <span class='right floated edit icon'>
-            <i class='edit icon'></i>
-          </span>
-        </div>
-      </div>
-      <div class='ui bottom attached green basic button' v-show="todo.done">
-        Completed
-      </div>
-      <div class='ui bottom attached red basic button' v-show="!todo.done">
-        Complete
-      </div>
-    </div>
+    <!-- we are now passing the data to the todo component to render the todo list -->
+    <todo v-on:delete-todo="deleteTodo" v-for="todo in todos" v-bind:todo="todo"></todo>
   </div>
 </template>
 
 <script type="text/javascript">
 
-export default {
-  props: ['todos']
-}
+import Todo from './Todo'
 
+export default {
+  props: ['todos'],
+  components: {
+    Todo
+  },
+  // TodoList component
+  methods: {
+    deleteTodo (todo) {
+      const todoIndex = this.todos.indexOf(todo)
+      this.todos.splice(todoIndex, 1)
+    }
+  }
+}
 </script>
 
 <style>
