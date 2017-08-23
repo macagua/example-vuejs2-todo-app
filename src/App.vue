5d0e5c3a90763560e2b598d76839dd539367500a
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2017-08-23 17:33:23 -0400

    Added CreateTodo component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:add-todo="addTodo"></create-todo>
   </div>
 </template>
 
 <script>
 import TodoList from './components/TodoList'
+import CreateTodo from './components/CreateTodo'
 
 export default {
   name: 'app',
   components: {
-    // Add a reference to the TodoList component in the components property
-    TodoList
+    // Add a reference to the TodoList, CreateTodo components in the components property
+    TodoList,
+    CreateTodo
   },
   // data function avails data to the template
   data () {
@@ -33,6 +36,14 @@ export default {
         project: 'Project D',
         done: false
       }]
+    }
+  },
+  methods: {
+    addTodo (title) {
+      this.todos.push({
+        title,
+        done: false
+      })
     }
   }
 }
