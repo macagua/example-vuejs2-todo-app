71e7e6d14bdce04329a9a31bd17ccae52280e266
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2017-08-23 13:34:26 -0400

    Added semantic-ui-css resources and a custom template for TodoList component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
 
@@ -12,6 +12,28 @@ export default {
   components: {
     // Add a reference to the TodoList component in the components property
     TodoList
+  },
+  // data function avails data to the template
+  data () {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false
+      }]
+    }
   }
 }
 </script>
